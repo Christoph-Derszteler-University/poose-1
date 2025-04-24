b9620f075964491773f38c98ba4be20b079d010e
Christoph Derszteler <s74cders@uni-bonn.de>
2025-04-24 13:03:35 +0200

    Fix SRC_FILES glob in CMakeLists.txt

--- examples/associative/hash_map_example.cpp
+++ examples/associative/hash_map_example.cpp
@@ -5,7 +5,7 @@ constexpr std::hash<std::string> hash_function;
 
 int main() {
   auto container = containers::associative::hash_map<std::string, std::string>(hash_function);
-  auto exists_before = container.find_by_key("test");
+  const auto exists_before = container.find_by_key("test");
   std::cout << "exists before inserting: " << exists_before.has_value() << std::endl;
 
   container.insert("test", "indeed");
@@ -13,6 +13,6 @@ int main() {
   std::cout << " (Container size: " << container.size() << ")" << std::endl;
 
   container.remove("test");
-  auto exists_remove = container.find_by_key("test");
+  const auto exists_remove = container.find_by_key("test");
   std::cout << "exists after removing: " << exists_remove.has_value() << std::endl;
 }
