4dd7cd7d5da1096e1b41fe16491b2e02fd97120d
helmerj1 <s43jhelm@uni-bonn.de>
2025-04-28 13:18:44 +0200

    Fixed various bugs e.g.  name issues with list and destruktor, project compiles now c: #3

--- include/sequential/list_stack.hpp
+++ include/sequential/list_stack.hpp
@@ -8,8 +8,8 @@ namespace containers::sequential {
     template <typename T>
     class list_stack: public abstract_stack<T> {
     private:
-        auto list;
-        size_t size;
+        std::list<T> m_list;
+        size_t m_size;
     public:
         /**
          * @brief constructs a stack using the list structure
@@ -17,16 +17,16 @@ namespace containers::sequential {
         list_stack();
 
         //! @copydoc abstract_stack::push
-        void push(const T& data);
+        void push(const T& data) override;
 
         //! @copydoc abtract_stack::pop
-        T pop();
+        T pop() override;
 
         //! @copydoc abstract_stack::isEmpty
-        bool isEmpty();
+        bool isEmpty() override;
 
         //! @copydoc abstract_stack::size
-        size_t size();
+        const size_t size() override;
 
     };
 }
