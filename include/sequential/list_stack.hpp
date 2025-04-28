ee65d75265fb27baa0e59c061e3ae984e42830c2
helmerj1 <s43jhelm@uni-bonn.de>
2025-04-28 09:43:09 +0200

    added list_stack.hpp #3

---
+++ include/sequential/list_stack.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "sequential/abstract_stack.hpp"
+#include <list>
+
+namespace containers::sequential {
+
+    template <typename T>
+    class list_stack: public abstract_stack<T> {
+    private:
+        auto list;
+    public:
+        /**
+         * @brief constructs a stack using the list structure
+         */
+        list_stack();
+
+        //! @copydoc abstract_stack::push
+        void push(const T& data);
+
+        //! @copydoc abtract_stack::pop
+        T pop();
+
+        //! @copydoc abstract_stack::isEmpty
+        bool isEmpty();
+
+        //! @copydoc abstract_stack::size
+        size_t size();
+
+    };
+}
+
+#include "inline/list_stack.tpp"
