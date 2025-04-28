56cacbd3774e68e58d25745c50d68b7f0b0516b8
helmerj1 <s43jhelm@uni-bonn.de>
2025-04-28 12:23:49 +0200

    - fixed bug were size() method was'nt correctly inherited. - changed attribute names in list_stack class #3

---
+++ examples/sequential/stack_example.cpp
@@ -0,0 +1,23 @@
+#include "include/sequential/list_stack.hpp"
+#include <iostream>
+
+using namespace containers::sequential;
+    
+int main() {
+    list_stack<int> my_int_stack;
+    my_int_stack.push(42);
+    my_int_stack.push(71);
+    my_int_stack.push(9);
+
+    size_t size = my_int_stack.size();
+    std::cout << "Die Größe hinter dem ':' sollte drei sein: " << size << std::endl;
+
+    int vorne = my_int_stack.pop();
+    std::cout << "Das oberste Element auf dem Stack war " << vorne << std::endl;
+
+    if (my_int_stack.isEmpty()) {
+        std::cout << "Der Stack ist leer" << std::endl;
+    } else {
+        std::cout << "Der Stack ist noch nicht leer" << std::endl;
+    }
+}
--- include/sequential/inline/list_stack.tpp
+++ include/sequential/inline/list_stack.tpp
@@ -7,17 +7,19 @@ namespace containers::sequential {
         size = 0;
     }
     template <typename T> void list_stack<T>::push(const T& data) {
-        list.push_back(data);
+        list.push_front(data);
         size ++;
     }
     template <typename T> T list_stack<T>::pop() {
-        return list.pop_front();
+        T tmp = list.front();
+        list.pop_front();
         size += -1;
+        return tmp;
     }
     template <typename T> bool list_stack<T>::isEmpty() {
         return (size == 0);
     }
-    template <typename T> size_t list_stack<T>::size() {
+    template <typename T> const size_t list_stack<T>::size() {
         return size;
     }
 
