7d1cd28d5e870e92a24511a73c04b0423204e589
KP64 <karamalsadeh@hotmail.com>
2025-04-29 20:32:25 +0200

    chore: do not use acronyms

--- examples/linked_list.cpp
+++ examples/linked_list.cpp
@@ -2,17 +2,17 @@
 #include <sequential/linked_list.hpp>
 
 int main() {
-  containers::sequential::linked_list<int> l;
+  containers::sequential::linked_list<int> list;
 
-  l.push_front(5);
-  l.push_front(10);
-  const auto head = l.front();
-  l.erase_after(head);
-  auto tail = l.insert_after(head, 40);
+  list.push_front(5);
+  list.push_front(10);
+  const auto head = list.front();
+  list.erase_after(head);
+  auto tail = list.insert_after(head, 40);
 
-  std::cout << l.size() << std::endl;
+  std::cout << list.size() << std::endl;
 
-  for (auto node = l.front(); node != nullptr; node = node->next) {
+  for (auto node = list.front(); node != nullptr; node = node->next) {
     std::cout << node->data << " ";
   }
 }
