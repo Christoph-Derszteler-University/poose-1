22098211c54c33bae77953215f2c5f1d545b591c
helmerj1 <s43jhelm@uni-bonn.de>
2025-05-01 17:48:41 +0200

    replaced stack_test.cpp with queue_test.cpp

---
+++ tests/sequential/queue_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include  "sequential/list_queue.hpp"
+
+class list_queue_test : public testing::Test {
+    protected:
+      using value_t = int;
+      using list_queue_t = containers::sequential::list_queue<value_t>;
+    
+      list_queue_t list_queue;
+    
+      list_queue_test() : list_queue() {}
+    
+      void SetUp() override {
+        list_queue.enqueue(42);
+        list_queue.enqueue(69);
+        list_queue.enqueue(2042006);
+      }
+    };
+
+TEST_F(list_queue_test, CorrectContainerSize){
+    EXPECT_EQ(list_queue.size(), 3);
+}
+TEST_F(list_queue_test, DataIsInsertedCorrectly){
+    list_queue.enqueue(45);
+    EXPECT_EQ(list_queue.front(), 45);
+}
+TEST_F(list_queue_test, DataIsDeletedCorrectly){
+    const auto&  tmp = list_queue.dequeue();
+    EXPECT_EQ(tmp, 45);
+    EXPECT_EQ(list_queue.size(), 3);
+    EXPECT_FALSE(list_queue.empty());
+}
+TEST_F(list_queue_test, QueueIsEmptyCorrectBehaviour){
+    list_queue.dequeue();
+    list_queue.dequeue();
+    list_queue.dequeue();
+
+    EXPECT_TRUE(list_queue.empty());
+    ASSERT_THROW(list_queue.dequeue(), containers::sequential::empty_container);
+}
