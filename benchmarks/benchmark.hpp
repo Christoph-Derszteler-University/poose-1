8038b4e02189cefcd1882572702c8deb6faf75df
helmerj1 <s43jhelm@uni-bonn.de>
2025-05-08 15:32:33 +0200

    Copy pasted Chris's new benchmark implementation to be able to start working

--- benchmarks/benchmark.hpp
+++ benchmarks/benchmark.hpp
@@ -4,6 +4,16 @@
 #include <chrono>
 
 namespace containers::benchmark {
-  void print_benchmark(const std::function<void()>& action, const std::string& name);
+  void print_benchmark(
+    const std::function<void()>& action,
+    const std::string& name,
+    const std::string& action_name,
+    const int& size
+  );
   std::chrono::duration<double> benchmark_action(const std::function<void()>& action);
+
+  void benchmark_with_different_sizes(
+    const std::function<void(const int&)>& benchmark,
+    const std::vector<int>& sizes
+  );
 }
